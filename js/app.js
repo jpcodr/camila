c2e15725b07c3eab3ccd1da460aebef1d648e4e4
Juan Pablo Macias Ramos <jupamara@gmail.com>
2015-05-21 20:18:06 -0500

    cambios diseño

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngMap'])
+angular.module('myApp', ['ngMap', 'matchMedia'])
 .controller('LocationCtrl', ['$scope', '$interval', function ($scope, $interval){
 
 	$scope.currentEvent = {
@@ -10,12 +10,20 @@ angular.module('myApp', ['ngMap'])
 		misa: {
 			name: 'Misa',
 			lat: 20.6220693,
-			lon: -103.3057118
+			lon: -103.3057118,
+			place: 'Parroquia Santos Mártires Mexicanos',
+			dir: 'San Pedro Apostol #887 Col. Lomas de San Miguel',
+			hora: '13:00 hrs.',
+			fecha: '23 de Mayo 2015'
 		},
 		fiesta: {
 			name: 'Fiesta',
 			lat: 20.618406,
-			lon: -103.291089
+			lon: -103.291089,
+			place: 'Terraza',
+			dir: 'Pemex # Col. San Pedrito',
+			hora: '15:00 hrs.',
+			fecha: '23 de Mayo 2015'
 		}
 	};
 
